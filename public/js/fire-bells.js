238df220f60e43404d25064f4c39df970f181609
Aleksandr Elmekeev <donamok@gmail.com>
2017-08-15 22:10:23 +0300

    added photos

--- public/js/fire-bells.js
+++ public/js/fire-bells.js
@@ -1,138 +1,173 @@
+var MAP_CENTER_LAT = 56.632057;
+var MAP_CENTER_LNG = 47.882995;
+var MAP_ZOOM = 8;
+
 var THUMBNAIL_SIZE = 40;
-var THUMBNAIL_SPRITE = 'https://dl.dropboxusercontent.com/u/14032692/blog/Fire-bells/thumbnails/firebellsprite.png';
+var THUMBNAIL_SPRITE = 'images/thumbnails/sprite.png';
 
 var data = [{
+  name: 'пгт Новый Торъял',
   lat: 57.018685,
   lng: 48.723789,
-  link: 'http://fire-bells.blogspot.ru/2014/03/blog-post.html'
+  link: 'images/1.jpg'
 }, {
+  name: 'д. Котёново',
   lat: 56.385812,
   lng: 46.868391,
-  link: 'http://fire-bells.blogspot.ru/2014/07/blog-post.html'
+  link: 'images/2.jpg'
 }, {
+  name: 'д. Умятеево',
   lat: 56.411598,
   lng: 46.863652,
-  link: 'http://fire-bells.blogspot.ru/2014/07/blog-post_4.html'
+  link: 'images/3.jpg'
 }, {
+  name: 'д. Яныково',
   lat: 56.623333,
   lng: 47.771667,
-  link: 'http://fire-bells.blogspot.ru/2013/09/blog-post_6693.html'
+  link: 'images/4.jpg'
 }, {
+  name: 'д. Ноля-Вершина',
   lat: 56.616833,
   lng: 47.787667,
-  link: 'http://fire-bells.blogspot.ru/2013/09/blog-post_23.html'
+  link: 'images/5.jpg'
 }, {
+  name: 'д. Крутой Овраг',
   lat: 56.633667,
   lng: 47.741833,
-  link: 'http://fire-bells.blogspot.ru/2013/09/blog-post_24.html'
+  link: 'images/6.jpg'
 }, {
+  name: 'д. Нолька',
   lat: 56.640833,
   lng: 47.695000,
-  link: 'http://fire-bells.blogspot.ru/2013/09/blog-post_25.html'
+  link: 'images/7.jpg'
 }, {
+  name: 'д. Ореховка',
   lat: 56.634667,
   lng: 47.682667,
-  link: 'http://fire-bells.blogspot.ru/2013/09/blog-post_26.html'
+  link: 'images/8.jpg'
 }, {
+  name: 'д. Юркино',
   lat: 56.513264,
   lng: 46.222819,
-  link: 'http://fire-bells.blogspot.ru/2014/08/blog-post.html'
+  link: 'images/9.jpg'
 }, {
+  name: 'с. Марьино',
   lat: 56.570790,
   lng: 46.011371,
-  link: 'http://fire-bells.blogspot.ru/2014/08/blog-post_16.html'
+  link: 'images/10.jpg'
 }, {
+  name: 'д. Сенюшкино',
   lat: 56.416818,
   lng: 46.718342,
-  link: 'http://fire-bells.blogspot.ru/2014/08/blog-post_84.html'
+  link: 'images/11.jpg'
 }, {
+  name: 'д. Воробьи',
   lat: 56.957222,
   lng: 47.864584,
-  link: 'http://fire-bells.blogspot.ru/2014/05/blog-post.html'
+  link: 'images/12.jpg'
 }, {
+  name: 'пгт Новый Торъял',
   lat: 56.998928,
   lng: 48.719967,
-  link: 'http://fire-bells.blogspot.ru/2013/10/blog-post.html'
+  link: 'images/13.jpg'
 }, {
+  name: 'с. Горняк',
   lat: 56.860162,
   lng: 49.054310,
-  link: 'http://fire-bells.blogspot.ru/2014/09/blog-post.html'
+  link: 'images/14.jpg'
 }, {
+  name: 'сады «Дружба»',
   lat: 56.587025,
   lng: 47.783367,
-  link: 'http://fire-bells.blogspot.ru/2014/05/blog-post_26.html'
+  link: 'images/15.jpg'
 }, {
+  name: 'д. Митюково',
   lat: 56.613267,
   lng: 47.816094,
-  link: 'http://fire-bells.blogspot.ru/2014/05/blog-post_27.html'
+  link: 'images/16.jpg'
 }, {
+  name: 'д. Верхнее Азяково',
   lat: 56.624716,
   lng: 47.625608,
-  link: 'http://fire-bells.blogspot.ru/2014/04/blog-post.html'
+  link: 'images/17.jpg'
 }, {
+  name: 'д. Сенькино',
   lat: 56.677333,
   lng: 47.915000,
-  link: 'http://fire-bells.blogspot.ru/2013/09/blog-post.html'
+  link: 'images/18.jpg'
 }, {
+  name: 'д. Большой Шаплак',
   lat: 56.710667,
   lng: 47.904667,
-  link: 'http://fire-bells.blogspot.ru/2013/09/blog-post_16.html'
+  link: 'images/19.jpg'
 }, {
+  name: 'д. Юркино',
   lat: 56.702333,
   lng: 47.926667,
-  link: 'http://fire-bells.blogspot.ru/2013/09/blog-post_17.html'
+  link: 'images/20.jpg'
 }, {
+  name: 'д. Малая Орша',
   lat: 56.908619,
   lng: 47.839890,
-  link: 'http://fire-bells.blogspot.ru/2014/10/blog-post_27.html'
+  link: 'images/21.jpg'
 }, {
+  name: 'д. Дементьево',
   lat: 56.885548,
   lng: 48.927828,
-  link: 'http://fire-bells.blogspot.ru/2014/10/blog-post.html'
+  link: 'images/22.jpg'
 }, {
+  name: 'с. Нурма',
   lat: 56.705500,
   lng: 47.712833,
-  link: 'http://fire-bells.blogspot.ru/2013/09/blog-post_18.html'
+  link: 'images/23.jpg'
 }, {
+  name: 'д. Нефедкино',
   lat: 56.700500,
   lng: 47.696333,
-  link: 'http://fire-bells.blogspot.ru/2013/09/blog-post_19.html'
+  link: 'images/24.jpg'
 }, {
+  name: 'д. Данилкино',
   lat: 56.692000,
   lng: 47.713333,
-  link: 'http://fire-bells.blogspot.ru/2013/09/blog-post_20.html'
+  link: 'images/25.jpg'
 }, {
+  name: 'д. Большой Яшнур',
   lat: 56.684000,
   lng: 47.729333,
-  link: 'http://fire-bells.blogspot.ru/2013/09/blog-post_1317.html'
+  link: 'images/26.jpg'
 }, {
+  name: 'п. Зелёная Роща',
   lat: 56.515790,
   lng: 48.336888,
-  link: 'http://fire-bells.blogspot.ru/2016/04/blog-post.html'
+  link: 'images/27.jpg'
 }, {
+  name: 'с. Азаново',
   lat: 56.719122,
   lng: 48.223535,
-  link: 'http://fire-bells.blogspot.ru/2016/04/blog-post_14.html'
+  link: 'images/28.jpg'
 }, {
+  name: 'д. Малый Шаплак',
   lat: 56.7666111,
   lng: 47.9323097,
-  link: 'http://fire-bells.blogspot.ru/2016/05/blog-post.html'
+  link: 'images/29.jpg'
 }, {
+  name: 'д. Савкино Поле',
   lat: 56.77299,
   lng: 47.9026839,
-  link: 'http://fire-bells.blogspot.ru/2016/05/blog-post_3.html'
+  link: 'images/30.jpg'
 }, {
+  name: 'д. Акиндулкино',
   lat: 56.7745678,
   lng: 47.9663981,
-  link: 'http://fire-bells.blogspot.ru/2016/05/blog-post_83.html'
+  link: 'images/31.jpg'
 }];
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
+    zoom: MAP_ZOOM,
     center: {
-      lat: 56.632057,
-      lng: 47.882995
+      lat: MAP_CENTER_LAT,
+      lng: MAP_CENTER_LNG
     }
   });
 
@@ -157,7 +192,7 @@ function initMap() {
 
   function addListeners(marker, bell) {
     marker.addListener('click', function() {
-      window.open(bell.link);
+      // todo
     });
   }
 }
